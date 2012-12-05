7cbf28dfecdf0a78bbf5f00a3a9a73e8d7ede756
Keyvan Fatehi <keyvan@mdks.org>
2012-12-04 18:43:29 -0800

    trying to hide kb but facing other issues now

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -16,18 +16,24 @@ class SettingsController < UIViewController
 
   def viewDidLoad
     # Load vars from Persistence
-  end
-
-  def viewDidUnload
-    # Save vars into Persistence
+    # address.returnKeyType = UIReturnKeyDone;
+    # address.delegate = self;
+    # http://stackoverflow.com/questions/5151808/how-to-hide-keyboard-in-uiviewcontroller-on-return-button-click-iphone
+    # textFieldTwo.returnKeyType = UIReturnKeyDone;
+    # textFieldCislo.delegate = self;
   end
 
   def test(sender)
-    
+    # Test it, if it works, turn into "Save", which will apply to Persistence
+    # if it doesnt work, stay as Test
   end
 
   def back(sender)
     App.switch_to('ViewerController')
+  end
+
+  def textFieldShouldReturn(textfield)
+    textfield.resignFirstResponder
   end
 
 end
